87f4fb2db41e61ee498bb668a9ac698ac78f65e2
hnanhquoc <hnanhquoc@gmail.com>
2017-02-15 20:44:46 +0700

    Milestone 4

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+photo = Photo.create([
+  {username: Faker::Name.name,
+  caption: Faker::Lorem.word,
+  likes_count: 10000,
+  url: Faker::Avatar.image,
+  created_at: Faker::Date.forward(1)}])
